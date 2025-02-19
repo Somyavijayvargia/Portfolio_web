ad7330d7a4c7e36dd49802058f1c8cf5dd5d4807
Somyavijayvargia <somyavijayvargia@gmail.com>
2025-02-19 14:47:51 +0530

    Updated project files

---
+++ components/Hero.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    
+    <div className="pb-20 pt-36"></div>
+  )
+}
+
+export default Hero
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +9,14 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        black: {
+          DEFAULT: "#000",
+          100: "#000319",
+        },
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+
+export default config;
